34eb8e4ba9d0471a6c10658383a3587d39bdf5aa
Ned Schwartz <ned@theinterned.net>
2017-06-09 20:39:59 -0400

    basic icon styles

--- src/ui/Icon/Icon.js
+++ src/ui/Icon/Icon.js
@@ -1,11 +1,31 @@
 // eslint-disable import/no-webpack-loader-syntax
 import React from "react";
 import PropTypes from "prop-types";
+import { StyleSheet, css } from 'aphrodite';
 import BabyIcon from './icons/Baby.icon.svg';
 
-const Icon = ({ name }) => <div><BabyIcon /> {name}</div>;
+const Icon = ({ name }) => <span className={css(styles.icon)}>
+  <BabyIcon className={css(styles.svg)} />
+</span>;
+
 export default Icon;
 
 Icon.propTypes = {
   name: PropTypes.string,
 };
+
+const styles = StyleSheet.create({
+  icon:  {
+    display: 'inline-flex',
+    alignSelf: 'center',
+    position: 'relative',
+    height: '1em',
+    width: '1.25em'
+  },
+  svg: {
+    height: '1.25em',
+    width: '1.25em',
+    top: 0,
+    position: 'absolute'
+  }
+});
