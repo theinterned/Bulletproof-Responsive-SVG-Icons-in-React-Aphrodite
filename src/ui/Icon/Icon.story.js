923cbbbdb8acb77fb6a698eb8ce831c9572deae3
Ned Schwartz <ned@theinterned.net>
2017-06-09 23:04:14 -0400

    adds a story showing the icons at different sizes

--- src/ui/Icon/Icon.story.js
+++ src/ui/Icon/Icon.story.js
@@ -16,6 +16,70 @@ storiesOf('Icon', module)
       <Text><Icon name="Tooth" /> Tooth Icon</Text>
     </div>
   ))
+  .add('at different sizes', () => (
+    <div>
+      <Text size="xs">
+        xs:
+        <Icon name="Baby" />
+        <Icon name="Balloon" />
+        <Icon name="Globe" />
+        <Icon name="Knife" />
+        <Icon name="Skull" />
+        <Icon name="Tear" />
+        <Icon name="Tooth" />
+      </Text>
+      <Text size="sm">
+         sm:
+        <Icon name="Baby" />
+        <Icon name="Balloon" />
+        <Icon name="Globe" />
+        <Icon name="Knife" />
+        <Icon name="Skull" />
+        <Icon name="Tear" />
+        <Icon name="Tooth" />
+      </Text>
+      <Text size="md">
+        md:
+        <Icon name="Baby" />
+        <Icon name="Balloon" />
+        <Icon name="Globe" />
+        <Icon name="Knife" />
+        <Icon name="Skull" />
+        <Icon name="Tear" />
+        <Icon name="Tooth" />
+      </Text>
+      <Text size="lg">
+        lg:
+        <Icon name="Baby" />
+        <Icon name="Balloon" />
+        <Icon name="Globe" />
+        <Icon name="Knife" />
+        <Icon name="Skull" />
+        <Icon name="Tear" />
+        <Icon name="Tooth" />
+      </Text>
+      <Text size="xl">
+        xl:
+        <Icon name="Baby" />
+        <Icon name="Balloon" />
+        <Icon name="Globe" />
+        <Icon name="Knife" />
+        <Icon name="Skull" />
+        <Icon name="Tear" />
+        <Icon name="Tooth" />
+      </Text>
+      <Text size="xxl">
+        xxl:
+        <Icon name="Baby" />
+        <Icon name="Balloon" />
+        <Icon name="Globe" />
+        <Icon name="Knife" />
+        <Icon name="Skull" />
+        <Icon name="Tear" />
+        <Icon name="Tooth" />
+      </Text>
+    </div>
+  ))
   .add('In a block of text', () => (
     <Text><Icon name="Balloon" /> lipsumSed velit mentitum, incurreret eu quorum,
     incididunt litteris si iudicem cillum dolor ea duis eu proident enim quis an
