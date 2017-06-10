a2b7a025cfdd692fc9854b5130aeda85d99ae789
Ned Schwartz <ned@theinterned.net>
2017-06-09 23:06:02 -0400

    clean up

--- src/elements/Text.js
+++ src/elements/Text.js
@@ -2,23 +2,22 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { StyleSheet, css } from 'aphrodite';
 
-const sizes = ['xs', 'sm', 'md', 'lg', 'xl', 'xxl'];
-
 const Text = ({children, size = 'md'}) => <p className={css(styles.text, styles[size])}>{children}</p>;
 
 export default Text;
 
-Text.propTypes = {
-  children: PropTypes.node,
-  size: PropTypes.oneOf(sizes)
-}
-
+const sizes = ['xs', 'sm', 'md', 'lg', 'xl', 'xxl'];
 const baseFontSize = 16;
 const baseLineHeight = 20;
 
 const fontSize = (target = baseFontSize, base = baseFontSize) => target / base;
 const lineHeight = (target = baseLineHeight, fontSize = baseFontSize) => target / fontSize;
 const em = measure => `${measure}em`;
+
+Text.propTypes = {
+  children: PropTypes.node,
+  size: PropTypes.oneOf(sizes)
+}
 
 export const textStyles = {
   fontSize: em(fontSize()),
